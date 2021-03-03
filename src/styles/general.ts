1a9b2d4a179a5fa7009d6511b6de7f468ff366a7
Tatyna Dotsenko <userdotsenko@gmail.com>
2021-03-03 19:17:46 +0300

    + mobile

--- src/styles/general.ts
+++ src/styles/general.ts
@@ -20,10 +20,6 @@ export const ContainerRow = styled.div`
 export const ContainerRowAlignStart = styled(ContainerRow)`
   align-items: start;
   justify-content: center;
-  // @media (max-width: ${props => props.theme.mobile}) {
-  //   margin: 0 0 20px;
-  //   align-items: center;
-  // }
 `
 
 export const ContainerSpaceBetween = styled(ContainerRow)`
