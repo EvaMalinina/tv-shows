13be7381b03e452a92a3c44332063a9c1f76169c
EvaMalinina <userdotsenko@gmail.com>
2021-03-16 23:35:56 +0200

    +useCallback

--- src/components/films/components/filmsHeader/FilmsSorting.tsx
+++ src/components/films/components/filmsHeader/FilmsSorting.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SelectContainer, Sorting } from "./filmsHeader.styled";
 import SelectC from "../../../ui/Select/Select";
 
+
 type Option = {
   id: number;
   type: string;
@@ -9,11 +10,11 @@ type Option = {
 
 const _sortOptions: Option[] = [
   { id: 1, type: 'release date'},
-  { id: 2, type: 'more popular'},
-  { id: 3, type: 'less popular'},
+  { id: 2, type: 'movie rating'},
 ];
 
 const FilmsSorting = () => {
+
   return(
     <Sorting>
       <label>
