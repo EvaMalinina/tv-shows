68ea98dd7b00d11d18d57d96767b70af48a9141d
EvaMalinina <userdotsenko@gmail.com>
2021-04-01 18:00:27 +0400

    sagas all to fork

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,14 +1,12 @@
-import { all, call } from 'redux-saga/effects'
+import { fork } from 'redux-saga/effects'
 import watchFetchMovies from "../components/films/components/filmsList/store/sagas";
 import watchSendNewMovie, {watchDeleteMovie, watchUpdateMovie} from "../components/filmPopups/storeMovie/sagas";
 import watchShowAlert from "../components/filmPopups/storeAlerts/sagas";
 
 export default function* rootSaga() {
-  yield all([
-    call(watchFetchMovies),
-    call(watchSendNewMovie),
-    call(watchDeleteMovie),
-    call(watchShowAlert),
-    call(watchUpdateMovie)
-  ])
+  yield fork(watchFetchMovies),
+  yield fork(watchSendNewMovie),
+  yield fork(watchDeleteMovie),
+  yield fork(watchShowAlert),
+  yield fork(watchUpdateMovie)
 }
